be205acefd3a4502d52baeaf8513c6a2c296ed26
Pedro A. <pedroaugusto732@gmail.com>
2025-02-05 15:11:18 -0300

    Update UsedProduct.java

--- src/entities/UsedProduct.java
+++ src/entities/UsedProduct.java
@@ -1,4 +1,36 @@
 package entities;
 
-public class UsedProduct {
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class UsedProduct extends Product{
+
+    private LocalDate manufactureDate;
+
+    public UsedProduct() {
+        super();
+    }
+
+    public UsedProduct( String name, Double price, LocalDate manufactureDate) {
+        super(name, price);
+        this.manufactureDate = manufactureDate;
+    }
+
+    public LocalDate getManufactureDate() {
+        return manufactureDate;
+    }
+
+    public void setManufactureDate( LocalDate manufactureDate ) {
+        this.manufactureDate = manufactureDate;
+    }
+
+    @Override
+    public String pricetag() {
+        return getName()
+                + " (usado) # "
+                + String.format("%.2f", getPrice())
+                + " (Data de fabricação:"
+                + manufactureDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+                +") ";
+    }
 }
